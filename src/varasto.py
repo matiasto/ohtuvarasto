1dd6b1d27c0c97ded25299d54c8393a9fd215a10
Matias Tolppanen <matias.tolppanen@outlook.com>
2022-11-14 00:19:07 +0200

    feat(viikko2): tehtava3

--- src/varasto.py
+++ src/varasto.py
@@ -1,22 +1,24 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
+        self.tilavuus = 0.0
+        self.saldo = 0.0
+        self.aseta_tilavuus(tilavuus)
+        self.aseta_saldo(alku_saldo)
+
+    def aseta_tilavuus(self, tilavuus):
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
 
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
+    def aseta_saldo(self, saldo):
+        self.saldo = self.tilavuus
+        if saldo < 0.0:
             self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
+        elif saldo <= self.tilavuus:
             # mahtuu
-            self.saldo = alku_saldo
-        else:
-            # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
+            self.saldo = saldo
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
